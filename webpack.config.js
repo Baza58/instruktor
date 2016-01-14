5c2b820967f53614aca97a590690249d59949377
Petr Bažout <bazout.p@gmail.com>
2016-01-14 11:54:18 +0100

    minify bundle

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
 		]
 	},
 	plugins: [
-  			// new webpack.optimize.UglifyJsPlugin({minimize: true}),
+  			new webpack.optimize.UglifyJsPlugin({minimize: true}),
  	],
 	postcss: [ autoprefixer({ browsers: ['last 2 versions'] }) ]
 }
